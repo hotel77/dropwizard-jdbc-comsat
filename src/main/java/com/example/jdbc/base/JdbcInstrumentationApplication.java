444c45f1f730ffb344ded71d89096555c9714567
Hyatt, Jamison <Jamison.Hyatt@capitalone.com>
2015-10-26 13:11:41 -0700

    updated to capsule and included quasar agent in it

--- src/main/java/com/example/jdbc/base/JdbcInstrumentationApplication.java
+++ src/main/java/com/example/jdbc/base/JdbcInstrumentationApplication.java
@@ -13,7 +13,6 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class JdbcInstrumentationApplication extends FiberApplication<JdbcInstrumentationConfiguration> {
-//public class JdbcInstrumentationApplication extends Application<JdbcInstrumentationConfiguration> {
     static final MetricRegistry metrics = new MetricRegistry();
 
     public static void main(String[] args) throws Exception {
@@ -30,27 +29,10 @@ public class JdbcInstrumentationApplication extends FiberApplication<JdbcInstrum
 
     }
 
-//    @Override
-//    public void run(JdbcInstrumentationConfiguration configuration, Environment environment) throws Exception {
-//        applicationSetup(configuration, environment);
-//    }
-//
-//    public void applicationSetup(JdbcInstrumentationConfiguration configuration, Environment environment) throws Exception {
-//
-//        final ManagedDataSource regularDataSource = configuration.getDatabase().build(metrics, "jdbc");
-//
-//
-//        final IAccountDAO accountDAO = new AccountDAO(regularDataSource);
-//        final IAccountDomain accountDomain = new AccountDomain(accountDAO);
-//        final AccountAPI accountInfoAPI = new AccountAPI(accountDomain);
-//
-//        environment.jersey().register(accountInfoAPI);
-//    }
 
     @Override
     public void fiberRun(JdbcInstrumentationConfiguration configuration, Environment environment) throws Exception {
         fiberApplicationSetup(configuration, environment);
-//        environment.getApplicationContext().setClassLoader(new QuasarWebAppClassLoader(environment.getApplicationContext()));
     }
 
     public void fiberApplicationSetup(JdbcInstrumentationConfiguration configuration, Environment environment) throws Exception {
@@ -58,9 +40,6 @@ public class JdbcInstrumentationApplication extends FiberApplication<JdbcInstrum
         final FiberDataSourceFactory fiberDataSourceFactory = new FiberDataSourceFactory(configuration.getDatabase());
         final ManagedDataSource fiberPssiDataSource = fiberDataSourceFactory.build(metrics, "jdbc");
 
-        //manual wrap
-//        final ManagedDataSource ds = configuration.getDatabase().build(metrics, "db");
-//        final ManagedDataSource fmds = FiberManagedDataSource.wrap(ds, Runtime.getRuntime().availableProcessors());
 
 
         final IAccountDAO accountDAO = new AccountDAO(fiberPssiDataSource);
